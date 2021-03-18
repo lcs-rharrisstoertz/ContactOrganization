d3926fa85ae0261de308de90317d8a00a932c582
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-03-18 08:39:12 -0400

    Initial Commit

---
+++ Shared/ContactOrganizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactOrganizationApp.swift
+//  Shared
+//
+//  Created by Harris-Stoertz, Rowan on 2021-03-18.
+//
+
+import SwiftUI
+
+@main
+struct ContactOrganizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
