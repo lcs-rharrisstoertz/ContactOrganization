acc87ba206effd3e3af406381f52d450ae44e845
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-03-18 08:42:57 -0400

    organized model and view files

---
+++ Shared/Model/Contact.swift
@@ -0,0 +1,12 @@
+//
+//  Contact.swift
+//  ContactOrganization (iOS)
+//
+//  Created by Harris-Stoertz, Rowan on 2021-03-18.
+//
+
+import Foundation
+
+struct Contact: Identifiable {
+    var id = UUID()
+}
