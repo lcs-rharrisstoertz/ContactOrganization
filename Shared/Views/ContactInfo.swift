1c5d5fcd220c3982302f52ff6bc3dd53b410ffc6
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-03-19 11:31:19 -0400

    Added labels to the different info in the detailed view

--- Shared/Views/ContactInfo.swift
+++ Shared/Views/ContactInfo.swift
@@ -12,35 +12,59 @@ struct ContactInfo: View {
     var contact: Contact
     
     var body: some View {
-        NavigationView {
-            ScrollView{
+            Form{
                 Group{
-                    Text(contact.nickname)
+                    Section(header: Text("Nickname")) {
+                        Text(contact.nickname)
+                    }
+                    Section(header: Text("Description")) {
                     Text(contact.description)
+                    }
+                    Section(header: Text("Phone number")) {
                     Text(contact.phoneNumber)
+                    }
+                    Section(header: Text("Email")) {
                     Text(contact.email)
+                    }
+                    Section(header: Text("Facebook")) {
                     Text(contact.facebook)
+                    }
+                    Section(header: Text("Twitter")) {
                     Text(contact.twitter)
+                    }
                 }
                 Group{
+                    Section(header: Text("YouTube")) {
                     Text(contact.youtube)
+                    }
+                    Section(header: Text("Instagram")) {
                     Text(contact.instagram)
+                    }
+                    Section(header: Text("Snapchat")) {
                     Text(contact.snapchat)
+                    }
+                    Section(header: Text("Tumblr")) {
                     Text(contact.tumblr)
+                    }
+                    Section(header: Text("Reddit")) {
                     Text(contact.reddit)
+                    }
+                    Section(header: Text("TikTok")) {
                     Text(contact.tiktok)
+                    }
+                    Section(header: Text("Other information")) {
                     Text(contact.otherInfo)
+                    }
                 }
             }
-        }
-        .navigationTitle(contact.name)
+            .navigationTitle(contact.name)
     }
 }
 
 struct ContactInfo_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
+        NavigationView{
             ContactInfo(contact: testStore.contacts[0])
-        }
+    }
     }
 }
