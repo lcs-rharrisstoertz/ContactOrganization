d329271cc1cd1051296b07c1b146057e214c0bd9
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-03-19 12:19:42 -0400

    updated detailed contact view- if a field contains an empty string, it's not shown

--- Shared/Views/ContactInfo.swift
+++ Shared/Views/ContactInfo.swift
@@ -16,46 +16,72 @@ struct ContactInfo: View {
     var body: some View {
         Form{
             Group{
-                Section(header: Text("Nickname")) {
-                    Text(contact.nickname)
+                if contact.nickname != "" {
+                    Section(header: Text("Nickname")) {
+                        Text(contact.nickname)
+                    }
                 }
-                Section(header: Text("Description")) {
-                    Text(contact.description)
+                if contact.description != "" {
+                    Section(header: Text("Description")) {
+                        Text(contact.description)
+                    }
                 }
-                Section(header: Text("Phone number")) {
-                    Text(contact.phoneNumber)
+                if contact.phoneNumber != "" {
+                    Section(header: Text("Phone number")) {
+                        Text(contact.phoneNumber)
+                    }
                 }
-                Section(header: Text("Email")) {
-                    Text(contact.email)
+                if contact.email != "" {
+                    Section(header: Text("Email")) {
+                        Text(contact.email)
+                    }
                 }
-                Section(header: Text("Facebook")) {
-                    Text(contact.facebook)
+                if contact.facebook != "" {
+                    Section(header: Text("Facebook")) {
+                        Text(contact.facebook)
+                    }
                 }
-                Section(header: Text("Twitter")) {
-                    Text(contact.twitter)
+                if contact.twitter != "" {
+                    Section(header: Text("Twitter")) {
+                        Text(contact.twitter)
+                    }
                 }
             }
             Group{
-                Section(header: Text("YouTube")) {
-                    Text(contact.youtube)
+                if contact.youtube != "" {
+                    Section(header: Text("YouTube")) {
+                        Text(contact.youtube)
+                    }
                 }
-                Section(header: Text("Instagram")) {
-                    Text(contact.instagram)
+                if contact.instagram != "" {
+                    Section(header: Text("Instagram")) {
+                        Text(contact.instagram)
+                    }
                 }
-                Section(header: Text("Snapchat")) {
-                    Text(contact.snapchat)
+                if contact.snapchat != "" {
+                    Section(header: Text("Snapchat")) {
+                        Text(contact.snapchat)
+                    }
                 }
-                Section(header: Text("Tumblr")) {
-                    Text(contact.tumblr)
+                if contact.tumblr != "" {
+                    Section(header: Text("Tumblr")) {
+                        Text(contact.tumblr)
+                    }
                 }
-                Section(header: Text("Reddit")) {
-                    Text(contact.reddit)
+                if contact.reddit != "" {
+                    Section(header: Text("Reddit")) {
+                        Text(contact.reddit)
+                    }
                 }
-                Section(header: Text("TikTok")) {
-                    Text(contact.tiktok)
+                if contact.tiktok != "" {
+                    Section(header: Text("TikTok")) {
+                        Text(contact.tiktok)
+                    }
                 }
-                Section(header: Text("Other information")) {
-                    Text(contact.otherInfo)
+                if contact.otherInfo != "" {
+                    Section(header: Text("Other information")) {
+                        Text(contact.otherInfo)
+                    }
                 }
             }
         }
