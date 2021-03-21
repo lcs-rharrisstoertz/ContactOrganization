1701fbc5e8e06e70495e264567a9d362debf157d
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-03-21 16:45:29 -0400

    added picture to detailed view

--- Shared/Views/ContactInfo.swift
+++ Shared/Views/ContactInfo.swift
@@ -15,6 +15,14 @@ struct ContactInfo: View {
     
     var body: some View {
         Form{
+            HStack{
+                Spacer()
+                Image(contact.picture)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                Spacer()
+            }
             Group{
                 if contact.nickname != "" {
                     Section(header: Text("Nickname")) {
