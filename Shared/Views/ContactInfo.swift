567322a1ae28cd6e5580c5c4fb1c59fffba3e884
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-03-21 15:56:20 -0400

    added buttons to open contact's profile for different info types (wasn't able to add button for all- some platforms don't have profile urls based on usernames)

--- Shared/Views/ContactInfo.swift
+++ Shared/Views/ContactInfo.swift
@@ -15,89 +15,147 @@ struct ContactInfo: View {
     
     var body: some View {
         Form{
-            if contact.email != "" {
-                Section(header: Text("Email")) {
-                    HStack {
-                        Text(contact.email)
-                        Button(action: {
-                            if let url = URL(string: "mailto:\(contact.email)") {
-                                UIApplication.shared.open(url)
-                            }
-                        }, label: {
-                            Text("Open")
-                        })
+            Group{
+                if contact.nickname != "" {
+                    Section(header: Text("Nickname")) {
+                        Text(contact.nickname)
+                    }
+                }
+                if contact.description != "" {
+                    Section(header: Text("Description")) {
+                        Text(contact.description)
+                    }
+                }
+                if contact.phoneNumber != "" {
+                    Section(header: Text("Phone number")) {
+                        HStack {
+                            Text(contact.phoneNumber)
+                            Button(action: {
+                                if let url = URL(string: "tel://\(contact.phoneNumber)") {
+                                    UIApplication.shared.open(url)
+                                }
+                            }, label: {
+                                Text("Open")
+                            })
+                        }
+                    }
+                }
+                if contact.email != "" {
+                    Section(header: Text("Email")) {
+                        HStack {
+                            Text(contact.email)
+                            Button(action: {
+                                if let url = URL(string: "mailto:\(contact.email)") {
+                                    UIApplication.shared.open(url)
+                                }
+                            }, label: {
+                                Text("Open")
+                            })
+                        }
+                    }
+                }
+                if contact.facebook != "" {
+                    Section(header: Text("Facebook")) {
+                        HStack {
+                            Text(contact.facebook)
+                            Button(action: {
+                                if let url = URL(string: "facebook.com/\(contact.facebook)/") {
+                                    UIApplication.shared.open(url)
+                                }
+                            }, label: {
+                                Text("Open")
+                            })
+                        }
+                    }
+                }
+                if contact.twitter != "" {
+                    Section(header: Text("Twitter")) {
+                        HStack {
+                            Text(contact.twitter)
+                            Button(action: {
+                                if let url = URL(string: "twitter.com/\(contact.twitter)/") {
+                                    UIApplication.shared.open(url)
+                                }
+                            }, label: {
+                                Text("Open")
+                            })
+                        }
                     }
                 }
             }
-//            Group{
-//                if contact.nickname != "" {
-//                    Section(header: Text("Nickname")) {
-//                        Text(contact.nickname)
-//                    }
-//                }
-//                if contact.description != "" {
-//                    Section(header: Text("Description")) {
-//                        Text(contact.description)
-//                    }
-//                }
-//                if contact.phoneNumber != "" {
-//                    Section(header: Text("Phone number")) {
-//                        Text(contact.phoneNumber)
-//                    }
-//                }
-//                if contact.email != "" {
-//                    Section(header: Text("Email")) {
-//                        Text(contact.email)
-//                    }
-//                }
-//                if contact.facebook != "" {
-//                    Section(header: Text("Facebook")) {
-//                        Text(contact.facebook)
-//                    }
-//                }
-//                if contact.twitter != "" {
-//                    Section(header: Text("Twitter")) {
-//                        Text(contact.twitter)
-//                    }
-//                }
-//            }
-//            Group{
-//                if contact.youtube != "" {
-//                    Section(header: Text("YouTube")) {
-//                        Text(contact.youtube)
-//                    }
-//                }
-//                if contact.instagram != "" {
-//                    Section(header: Text("Instagram")) {
-//                        Text(contact.instagram)
-//                    }
-//                }
-//                if contact.snapchat != "" {
-//                    Section(header: Text("Snapchat")) {
-//                        Text(contact.snapchat)
-//                    }
-//                }
-//                if contact.tumblr != "" {
-//                    Section(header: Text("Tumblr")) {
-//                        Text(contact.tumblr)
-//                    }
-//                }
-//                if contact.reddit != "" {
-//                    Section(header: Text("Reddit")) {
-//                        Text(contact.reddit)
-//                    }
-//                }
-//                if contact.tiktok != "" {
-//                    Section(header: Text("TikTok")) {
-//                        Text(contact.tiktok)
-//                    }
-//                }
-//                if contact.otherInfo != "" {
-//                    Section(header: Text("Other information")) {
-//                        Text(contact.otherInfo)
-//                    }
-//                }
-//            }
+            Group{
+                if contact.youtube != "" {
+                    Section(header: Text("YouTube")) {
+                        Text(contact.youtube)
+                    }
+                }
+                if contact.instagram != "" {
+                    Section(header: Text("Instagram")) {
+                        HStack {
+                            Text(contact.instagram)
+                            Button(action: {
+                                if let url = URL(string: "instagram.com/\(contact.instagram)/") {
+                                    UIApplication.shared.open(url)
+                                }
+                            }, label: {
+                                Text("Open")
+                            })
+                        }
+                    }
+                }
+                if contact.snapchat != "" {
+                    Section(header: Text("Snapchat")) {
+                        HStack {
+                            Text(contact.snapchat)
+                            Button(action: {
+                                if let url = URL(string: "snapchat.com/add/\(contact.snapchat)") {
+                                    UIApplication.shared.open(url)
+                                }
+                            }, label: {
+                                Text("Open")
+                            })
+                        }
+                    }
+                }
+                if contact.tumblr != "" {
+                    Section(header: Text("Tumblr")) {
+                        HStack {
+                            Text(contact.tumblr)
+                            Button(action: {
+                                if let url = URL(string: "\(contact.tumblr).tumblr.com") {
+                                    UIApplication.shared.open(url)
+                                }
+                            }, label: {
+                                Text("Open")
+                            })
+                        }
+                    }
+                }
+                if contact.reddit != "" {
+                    Section(header: Text("Reddit")) {
+                        HStack {
+                            Text(contact.reddit)
+                            Button(action: {
+                                if let url = URL(string: "reddit.com/user/\(contact.reddit)/") {
+                                    UIApplication.shared.open(url)
+                                }
+                            }, label: {
+                                Text("Open")
+                            })
+                        }
+                    }
+                }
+                if contact.tiktok != "" {
+                    Section(header: Text("TikTok")) {
+                        Text(contact.tiktok)
+                    }
+                }
+                if contact.otherInfo != "" {
+                    Section(header: Text("Other information")) {
+                        Text(contact.otherInfo)
+                    }
+                }
+            }
         }
         .navigationTitle(contact.name)
     }
