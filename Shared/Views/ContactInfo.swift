30ac563a7a328a9d44e8caebc795ee687c9c2a45
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-03-21 15:20:04 -0400

    added button that when clicked opens email/gmail w/ contact's email address filled in, letting the user quickly send an email to a contact

--- Shared/Views/ContactInfo.swift
+++ Shared/Views/ContactInfo.swift
@@ -15,75 +15,89 @@ struct ContactInfo: View {
     
     var body: some View {
         Form{
-            Group{
-                if contact.nickname != "" {
-                    Section(header: Text("Nickname")) {
-                        Text(contact.nickname)
-                    }
-                }
-                if contact.description != "" {
-                    Section(header: Text("Description")) {
-                        Text(contact.description)
-                    }
-                }
-                if contact.phoneNumber != "" {
-                    Section(header: Text("Phone number")) {
-                        Text(contact.phoneNumber)
-                    }
-                }
-                if contact.email != "" {
-                    Section(header: Text("Email")) {
+            if contact.email != "" {
+                Section(header: Text("Email")) {
+                    HStack {
                         Text(contact.email)
-                    }
-                }
-                if contact.facebook != "" {
-                    Section(header: Text("Facebook")) {
-                        Text(contact.facebook)
-                    }
-                }
-                if contact.twitter != "" {
-                    Section(header: Text("Twitter")) {
-                        Text(contact.twitter)
+                        Button(action: {
+                            if let url = URL(string: "mailto:\(contact.email)") {
+                                UIApplication.shared.open(url)
+                            }
+                        }, label: {
+                            Text("Open")
+                        })
                     }
                 }
             }
-            Group{
-                if contact.youtube != "" {
-                    Section(header: Text("YouTube")) {
-                        Text(contact.youtube)
-                    }
-                }
-                if contact.instagram != "" {
-                    Section(header: Text("Instagram")) {
-                        Text(contact.instagram)
-                    }
-                }
-                if contact.snapchat != "" {
-                    Section(header: Text("Snapchat")) {
-                        Text(contact.snapchat)
-                    }
-                }
-                if contact.tumblr != "" {
-                    Section(header: Text("Tumblr")) {
-                        Text(contact.tumblr)
-                    }
-                }
-                if contact.reddit != "" {
-                    Section(header: Text("Reddit")) {
-                        Text(contact.reddit)
-                    }
-                }
-                if contact.tiktok != "" {
-                    Section(header: Text("TikTok")) {
-                        Text(contact.tiktok)
-                    }
-                }
-                if contact.otherInfo != "" {
-                    Section(header: Text("Other information")) {
-                        Text(contact.otherInfo)
-                    }
-                }
-            }
+//            Group{
+//                if contact.nickname != "" {
+//                    Section(header: Text("Nickname")) {
+//                        Text(contact.nickname)
+//                    }
+//                }
+//                if contact.description != "" {
+//                    Section(header: Text("Description")) {
+//                        Text(contact.description)
+//                    }
+//                }
+//                if contact.phoneNumber != "" {
+//                    Section(header: Text("Phone number")) {
+//                        Text(contact.phoneNumber)
+//                    }
+//                }
+//                if contact.email != "" {
+//                    Section(header: Text("Email")) {
+//                        Text(contact.email)
+//                    }
+//                }
+//                if contact.facebook != "" {
+//                    Section(header: Text("Facebook")) {
+//                        Text(contact.facebook)
+//                    }
+//                }
+//                if contact.twitter != "" {
+//                    Section(header: Text("Twitter")) {
+//                        Text(contact.twitter)
+//                    }
+//                }
+//            }
+//            Group{
+//                if contact.youtube != "" {
+//                    Section(header: Text("YouTube")) {
+//                        Text(contact.youtube)
+//                    }
+//                }
+//                if contact.instagram != "" {
+//                    Section(header: Text("Instagram")) {
+//                        Text(contact.instagram)
+//                    }
+//                }
+//                if contact.snapchat != "" {
+//                    Section(header: Text("Snapchat")) {
+//                        Text(contact.snapchat)
+//                    }
+//                }
+//                if contact.tumblr != "" {
+//                    Section(header: Text("Tumblr")) {
+//                        Text(contact.tumblr)
+//                    }
+//                }
+//                if contact.reddit != "" {
+//                    Section(header: Text("Reddit")) {
+//                        Text(contact.reddit)
+//                    }
+//                }
+//                if contact.tiktok != "" {
+//                    Section(header: Text("TikTok")) {
+//                        Text(contact.tiktok)
+//                    }
+//                }
+//                if contact.otherInfo != "" {
+//                    Section(header: Text("Other information")) {
+//                        Text(contact.otherInfo)
+//                    }
+//                }
+//            }
         }
         .navigationTitle(contact.name)
     }
