54167ecc944e0630f2b50271f30b0474045db794
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-03-23 14:35:25 -0400

    created InstagramInfo struct

---
+++ Shared/Model/InstagramInfo.swift
@@ -0,0 +1,14 @@
+//
+//  InstagramInfo.swift
+//  ContactOrganization (iOS)
+//
+//  Created by Harris-Stoertz, Rowan on 2021-03-23.
+//
+
+import Foundation
+
+struct InstagramInfo: Decodable {
+    let id: String
+    let biography: String
+    let full_name: String
+}
